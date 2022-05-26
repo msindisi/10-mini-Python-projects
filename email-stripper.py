0a97baabf28541b592bbba2ab0b36f9502ca48a9
Msindisi Ndlela <msindisi.ndlela@gmail.com>
2022-05-26 06:51:40 +0200

    Create email-stripper.py

---
+++ email-stripper.py
@@ -0,0 +1,9 @@
+email = input("Enter Your Email: ").strip()
+
+username = email[:email.index("@")]
+
+domain_name = email[email.index("@")+1:]
+
+format_ = (f"Your username is '{username}' and your domain is 'www.{domain_name}'")
+
+print(format_)
